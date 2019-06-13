2569e4e5992cc7cf6fd68d461ef075cb42f69c3c
jupiter0811 <2402931605@qq.com>
2019-06-13 18:03:36 +0800

    hystrix

--- microservicecloud-consumer-dept-80/src/main/java/com/plwang/myrule/MyselfRule.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/plwang/myrule/MyselfRule.java
@@ -3,13 +3,33 @@ package com.plwang.myrule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.Server;
 
+/**
+ * 自定义ribbon负载均衡，需继承AbstractLoadBalancerRule，并重写choose方法
+ * @author Administrator
+ *
+ */
 @Configuration
-public class MyselfRule {
+public class MyselfRule extends AbstractLoadBalancerRule {
 	@Bean
 	public IRule myRule() {
 		return new RandomRule();
+	}
+
+	@Override
+	public Server choose(Object key) {
+		ILoadBalancer lb = getLoadBalancer();
+		// TODO 这里写负载均衡的逻辑
+		return null;
+	}
+
+	@Override
+	public void initWithNiwsConfig(IClientConfig clientConfig) {
 	}
 }
